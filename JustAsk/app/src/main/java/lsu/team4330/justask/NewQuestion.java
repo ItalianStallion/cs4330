7e068c93c7896909549275b90ace98e124d57fee
Ben Graham <graham.ben61@gmail.com>
2016-11-25 18:25:12 -0600

    Create RecipientListAdapter. Add stub for recipient list.

--- JustAsk/app/src/main/java/lsu/team4330/justask/NewQuestion.java
+++ JustAsk/app/src/main/java/lsu/team4330/justask/NewQuestion.java
@@ -3,7 +3,19 @@ package lsu.team4330.justask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+Class for creating new questions to send to selected users in recipientListView.
+ */
 public class NewQuestion extends AppCompatActivity {
 
     @Override
@@ -11,10 +23,37 @@ public class NewQuestion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_question);
 
+        // Creates toolbar and handles back navigation
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        EditText questionEditText = (EditText) findViewById(R.id.question_edit_text);
 
+        // String list stub for demoing
+        //TODO: Replace with an ArrayList of Users or user information
+        List<String> strings = new ArrayList<>();
+        String str = "Alan Turing";
+        strings.add(str);
+        String str2 = "Grace Hopper";
+        strings.add(str2);
+
+        // Adapts Users to Recipient List
+        ListView recipientListView = (ListView)findViewById(R.id.recipient_list_view);
+        RecipientListAdapter adapter = new RecipientListAdapter(this, R.layout.recipient_list_item, strings);
+        recipientListView.setAdapter(adapter);
+
+        // Retrieves a database reference
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+    }
+
+    public void sendQuestion(View view) {
+        //When send button is clicked
+    }
+
+    public void cancel(View view) {
+        // When cancel button is clicked
     }
 }
---
+++ JustAsk/app/src/main/java/lsu/team4330/justask/RecipientListAdapter.java
@@ -0,0 +1,48 @@
+package lsu.team4330.justask;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+/*
+Class for adapting JustAsk Users to the Recipient List
+TODO: When Facebook integration is finished, a user class should be defined and this should adapt List<User>
+ */
+public class RecipientListAdapter extends ArrayAdapter<String>{
+
+    private int layoutResource;
+
+    public RecipientListAdapter(Context context, int layoutResource, List<String> strings) {
+        super(context, layoutResource, strings);
+        this.layoutResource = layoutResource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+
+        if (view == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+            view = layoutInflater.inflate(layoutResource, null);
+        }
+
+        String str = (String) getItem(position);
+
+        if (str != null) {
+            TextView nameTextView = (TextView) view.findViewById(R.id.name_text_view);
+
+            if (nameTextView != null) {
+                nameTextView.setText(str);
+            }
+        }
+
+        return view;
+    }
+}
