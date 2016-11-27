22448c8ef3eb9ea3271dad2bbbb85616a7b9ea82
JohnEdwardSchwartzenburg <jeschwartzenburg@gmail.com>
2016-11-27 16:21:20 -0600

    facebook login implemented and working

---
+++ JustAsk/app/src/main/java/lsu/team4330/justask/Login.java
@@ -0,0 +1,61 @@
+package lsu.team4330.justask;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class Login extends FragmentActivity {
+    private CallbackManager callbackManager;
+    private TextView info;
+    private LoginButton loginButton;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.login);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        info = (TextView)findViewById(R.id.info);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel()
+            {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+                info.setText("Login attempt failed.");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
