7a0b6467d1debf8fa1367c56d6928f6638e2ab4c
Ben Graham <graham.ben61@gmail.com>
2016-11-27 22:28:42 -0600

    Random yes/no icons added and yes/no/undecided refactoring

--- JustAsk/app/src/main/java/lsu/team4330/justask/RecipientListAdapter.java
+++ JustAsk/app/src/main/java/lsu/team4330/justask/RecipientListAdapter.java
@@ -1,10 +1,13 @@
 package lsu.team4330.justask;
 
 import android.content.Context;
+import android.media.Image;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,6 +20,7 @@ TODO: When Facebook integration is finished, a user class should be defined and 
 public class RecipientListAdapter extends ArrayAdapter<String>{
 
     private int layoutResource;
+    // TODO: Replace and use database to load.
 
     public RecipientListAdapter(Context context, int layoutResource, List<String> strings) {
         super(context, layoutResource, strings);
@@ -33,15 +37,33 @@ public class RecipientListAdapter extends ArrayAdapter<String>{
             view = layoutInflater.inflate(layoutResource, null);
         }
 
-        String str = (String) getItem(position);
+        String name = (String) getItem(position);
 
-        if (str != null) {
+        if (name != null) {
             TextView nameTextView = (TextView) view.findViewById(R.id.name_text_view);
 
             if (nameTextView != null) {
-                nameTextView.setText(str);
+                nameTextView.setText(name);
+            }
+            if(layoutResource == R.layout.recipient_list_item_2) {
+                ImageView imageView = (ImageView) view.findViewById(R.id.image_view);
+
+                //TODO: use user_id to find response in question's recipient list in database
+                if(imageView != null) {
+                    Double rand = Math.random();
+                    if (rand <=0.33) {
+                        imageView.setImageResource(R.drawable.yes);
+                    }
+                    else if (rand <= 0.66) {
+                        imageView.setImageResource(R.drawable.no);
+                    }
+                    else {
+                        imageView.setVisibility(View.INVISIBLE);
+                    }
+                }
             }
         }
+
 
         return view;
     }
