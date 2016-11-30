7bc4939b5170ef3c6857d878ce0b5ac18edd37c4
Ben Graham <graham.ben61@gmail.com>
2016-11-29 19:37:18 -0600

    Firebase Authentication, logout, and delete account

--- JustAsk/app/src/main/java/lsu/team4330/justask/Login.java
+++ JustAsk/app/src/main/java/lsu/team4330/justask/Login.java
@@ -3,22 +3,38 @@ package lsu.team4330.justask;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //TODO: If logged in launch MainActivity, else launch Login.
 public class Login extends FragmentActivity {
+    private String TAG = "Login";
     private CallbackManager callbackManager;
     private TextView info;
     private LoginButton loginButton;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -27,11 +43,30 @@ public class Login extends FragmentActivity {
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
 
+        // If another activity redirected to login page, show toast with message.
+        Intent intent = getIntent();
+        if(intent.getStringExtra("MESSAGE") != null)
+            Toast.makeText(Login.this, intent.getStringExtra("MESSAGE"),
+                    Toast.LENGTH_SHORT).show();
+
         ImageView iv = (ImageView) findViewById(R.id.imageView);
         iv.setImageResource(R.drawable.justask_large_silhouette);
 
+        auth = FirebaseAuth.getInstance();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
 
-        loginButton = (LoginButton) findViewById(R.id.login_button);
 
         info = (TextView) findViewById(R.id.info);
         TextView justAsk = (TextView) findViewById(R.id.just_ask);
@@ -39,11 +74,13 @@ public class Login extends FragmentActivity {
         Typeface type = Typeface.createFromAsset(getAssets(),"fonts/Damion-Regular.ttf");
         justAsk.setTypeface(type);
 
+        loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
         {
             @Override
             public void onSuccess(LoginResult loginResult)
             {
+                handleFacebookAccessToken(loginResult.getAccessToken());
                 info.setText(
                         "User ID: "
                                 + loginResult.getAccessToken().getUserId()
@@ -70,8 +107,44 @@ public class Login extends FragmentActivity {
         });
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
     }
 }
--- JustAsk/app/src/main/java/lsu/team4330/justask/Settings.java
+++ JustAsk/app/src/main/java/lsu/team4330/justask/Settings.java
@@ -1,14 +1,22 @@
 package lsu.team4330.justask;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Settings extends AppCompatActivity {
+    private String TAG = "Settings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +32,33 @@ public class Settings extends AppCompatActivity {
 
     public void logOut(View v) {
         LoginManager.getInstance().logOut();
+        FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this, Login.class);
+        intent.putExtra("MESSAGE", "Logged out successfully");
         startActivity(intent);
+
         finish();
+    }
+
+    public void deleteAccount(View v) {
+        LoginManager.getInstance().logOut();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User account deleted.");
+
+                            Intent intent = new Intent(Settings.this, Login.class);
+                            intent.putExtra("MESSAGE", "Account Deleted");
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+
+
     }
 }
